8316f3f09b5de5bdf19324633cada60493dc48ba
Jordan Baron <jordannbaron@gmail.com>
2021-08-22 22:45:57 -0400

    Move deleteFile to here

--- src/firebase/firebaseFunctions.js
+++ src/firebase/firebaseFunctions.js
@@ -12,9 +12,17 @@ const projectFirestore = firebase.firestore()
 const createTimestamp = firebase.firestore.FieldValue.serverTimestamp
 const deleteField = firebase.firestore.FieldValue.delete
 
+const deleteFile = async(collection, id, fileName) => {
+  const storageRef = projectStorage.ref(fileName)
+  var dbRef = projectFirestore.collection(collection).doc(id)
+  await storageRef.delete()
+  await dbRef.delete()
+}
+
 export { 
   projectStorage,
   projectFirestore,
   createTimestamp,
   deleteField,
+  deleteFile
 }
